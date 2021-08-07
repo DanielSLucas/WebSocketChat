258878182439741af5ae6ead0f552b0404c80365
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2021-08-07 16:35:03 -0300

    build: socket.io

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import path from 'path';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
+
+const server = createServer(app);
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("Socket", socket);
+});
 
 app.get("/", (request, response) => {
   return response.json({ message: "Hello world!"})
 })
 
-app.listen(3333, () =>  console.log("Server is running on port 3333"));
+server.listen(3333, () =>  console.log("Server is running on port 3333"));
