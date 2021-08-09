cfadb5393d0f8dc34cc448f65aa829e8555c1b9c
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2021-08-09 10:44:21 -0300

    start chat event

--- src/websocket/ChatService.ts
+++ src/websocket/ChatService.ts
@@ -1,6 +1,7 @@
 import { container } from "tsyringe";
 import { io } from "../http";
 import { CreateUserService } from "../services/CreateUserService";
+import { GetAllUsersService } from "../services/GetAllUsersService";
 
 io.on("connect", socket => {
   socket.on("start", async (data) => {
@@ -14,6 +15,16 @@ io.on("connect", socket => {
       socket_id: socket.id,
     });
 
+    socket.broadcast.emit("new_users", user);
+
     console.log(user);
+  });
+
+  socket.on("get_users", async (callback) => {
+    const getAllUsersService = container.resolve(GetAllUsersService);
+
+    const users = await getAllUsersService.execute();
+
+    callback(users);
   });
 });
